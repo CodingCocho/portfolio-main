f2a95a0ac471f8c641539c961f5b7d2f67a632d5
CodingCocho <raulujr@gmail.com>
2022-01-12 23:12:56 -0800

    Updated meta information for social media link

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,19 +6,19 @@ const CustomHead = ({ title }) => {
       <title>{title}</title>
       <meta
         name="description"
-        content="Nitin Ranganath is an avid full stack web developer building websites and applications you'd love to use"
+        content="Raul Uriostegui is an avid full stack web developer building websites and applications you'd love to use"
       />
       <meta
         name="keywords"
-        content="nitin, nitin ranganath, ranganath, web developer, nitin web developer, nitin developer, mern stack, nitin portfolio"
+        content="raul, raul uriostegui, uriostegui, web developer, raul web developer, raul developer, mern stack, raul portfolio"
       />
-      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
+      <meta property="og:title" content="Raul Uriostegui's Portfolio" />
       <meta
         property="og:description"
         content="A full-stack developer building websites that you'd like to use."
       />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta property="og:image" content="https://imgur.com/HKF1hg9.png" />
+      <meta property="og:url" content="https://portfolio-main-mu.vercel.app/" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
